406d5e9a8b932a56a30287312d5611f529f38a53
Krzysiek <krzysztof_sroczyk@epam.com>
2018-04-27 16:24:26 +0200

    correted readme, adding mailmap file, remove commented code

--- src/test/java/xoGame/gameStates/GameInProgressTest.java
+++ src/test/java/xoGame/gameStates/GameInProgressTest.java
@@ -10,7 +10,6 @@ import xoGame.xoGameExceptions.TooManyArgumentsException;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.function.Supplier;
 
@@ -39,53 +38,5 @@ public class GameInProgressTest {
         Assert.assertTrue(gameState instanceof GameInProgress);
     }
 
-    //TODO  
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public static void testMoveToNextStateAndThrowIllegalArgumentException() throws TooManyArgumentsException {
-//        //given
-//        XOBoard xoBoard = XOBoard.parse("3 3");
-//        VictoryChecker victoryChecker = VictoryChecker.parse("4");
-//        ScoreBoard scoreBoard = new ScoreBoard();
-//
-//        String input = "some string";
-//        InputStream in = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(in);
-//
-//        Supplier<String> userInputProvider = new Scanner(System.in)::nextLine;
-//        Player player = Player.X;
-//
-//        GameState gameInProgress = new GameInProgress(player, xoBoard, victoryChecker, scoreBoard);
-//        gameInProgress.printTo(System.out::println);
-//
-//        //when and then
-//        gameInProgress.moveToNextState(userInputProvider);
-//    }
 
-//    @Test
-//    public void testMoveToNextStateMethodAndMoveToVictoryState() throws TooManyArgumentsException {
-//        //given
-//        XOBoard xoBoard = XOBoard.parse("3 3");
-//        VictoryChecker victoryChecker = VictoryChecker.parse("4");
-//        Player player = Player.X;
-//        ScoreBoard scoreBoard = new ScoreBoard();
-//
-//        // when
-//        GameState gameInProgress = new GameInProgress(player, xoBoard, victoryChecker, scoreBoard);
-//        for (int i = 0; i <= 8; i++) {
-//            Random random = new Random();
-//            int x = random.nextInt();
-//            int y = random.nextInt();
-//            String input = x + " " + y;
-//
-//            InputStream in = new ByteArrayInputStream(input.getBytes());
-//            System.setIn(in);
-//            Supplier<String> userInputProvider = new Scanner(System.in)::nextLine;
-//
-//
-//            gameInProgress = gameInProgress.moveToNextState(userInputProvider);
-//        }
-//
-//        // then
-//        Assert.assertTrue(gameInProgress instanceof EndOfTheGame);
-//    }
 }
